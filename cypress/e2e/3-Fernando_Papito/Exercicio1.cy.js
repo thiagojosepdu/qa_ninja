ffe379cc0ad3526c196fe94861866e96d45bea1d
Thiago Jose <josthiago1@gmail.com>
2022-11-08 17:28:38 -0300

    primeiro commit

---
+++ cypress/e2e/3-Fernando_Papito/Exercicio1.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('home page', () => {
+    it('app deve estar online', () => {
+        cy.viewport(1400, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        cy.get('#page-home > div >  main > h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
+    });
+})
